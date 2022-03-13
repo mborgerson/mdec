acb82b4df91d18de4136153e7424ef28f3d30d88
Matt Borgerson <contact@mborgerson.com>
2022-03-12 17:34:34 -0700

    Add simple test case

---
+++ tests/test.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import unittest
+import subprocess
+import tempfile
+import requests
+import os
+
+
+TEST_ROOT = os.path.abspath(os.path.dirname(__file__))
+
+
+class ServicesTest(unittest.TestCase):
+
+	@staticmethod
+	def _test_service(service_name: str, binary_path: str):
+		print(f'Testing {service_name}')
+		with open(binary_path, 'rb') as f:
+			r = requests.post(f'http://127.0.0.1/{service_name}/decompile',
+				              files={'file': f})
+			if r.status_code != 200:
+				r.raise_for_status()
+
+	def test_all_services(self):
+		with tempfile.TemporaryDirectory() as working_dir:
+			src_path = os.path.join(TEST_ROOT, 'src', 'fib.c')
+			bin_path = os.path.join(working_dir, 'fib')
+			subprocess.run(['gcc', '-o', bin_path, src_path], check=True)
+			for service_name in ['angr', 'reko', 'retdec', 'snowman']:
+				self._test_service(service_name, bin_path)
+
+
+if __name__ == '__main__':
+	unittest.main()
