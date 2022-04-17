e86c55bda94c16cb5bf6552d2c21332cc5d646e2
n01e0 <reoshiseki@gmail.com>
2022-04-17 16:26:13 +0900

    remove duplicate #include in outputs

--- backend/r2dec/mdec-r2dec/mdecr2dec/service.py
+++ backend/r2dec/mdec-r2dec/mdecr2dec/service.py
@@ -17,17 +17,19 @@ class R2decService(Service):
         r2.cmd('a'*6)
         funcs = [func['name'] for func in r2.cmdj('aflj')]
 
+        includes = set()
         out = []
 
         for func in funcs:
             try:
                 dec = r2.cmd(f'pdd @{func}')
-                out.append(dec.replace('/* r2dec pseudo code output */\n', '')) 
+                [includes.add(line + '\n') for line in dec.splitlines() if line.startswith('#include')]
+                out.append('\n'.join([line for line in dec.splitlines() if not line.startswith('#include')]).replace('/* r2dec pseudo code output */\n', '')+'\n')
             except:
                 out.append(f'/* Decompilation of {func} failed:\n{traceback.format_exc()}\n*/')
 
 
-        return '\n'.join(out)
+        return '\n'.join(includes) + '\n'.join(out)
 
     def version(self) -> str:
         return subprocess.run(['/usr/local/bin/r2', '-v'], stdout=subprocess.PIPE).stdout.splitlines()[0].split()[1].decode('utf-8', 'ignore')
